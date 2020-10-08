c3cc4fb2a1b5ff613c5aa5d890b88da1e86fa6de
Eugene <ewg1996@yandex.ru>
2020-10-08 23:07:51 +0500

    add class Tonnel

---
+++ Tonnel.java
@@ -0,0 +1,31 @@
+public class Tunnel extends Stage {
+    static Semaphore semaphore;
+
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    static {
+        semaphore = new Semaphore(MainClass.CARS_COUNT / 2);
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                semaphore.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+                semaphore.release();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
